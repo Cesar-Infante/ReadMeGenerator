2d223e316d7d43b6fd5e55d939354fc0cadece00
Cesar's PC <cesar261110@gmaill.com>
2022-07-21 12:26:19 -0700

    changes to questions array, write to file and init function implemented

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-
+/* This function is a series of questions that will be asked to the user. */
 const questions = () => {
     const myQuestions = inquirer.prompt([
         {
@@ -35,7 +36,7 @@ const questions = () => {
             type: "checkbox",
             message: "Does your current project have a license? ",
             name: 'licenses',
-            choices: ["Apache", "GNU", "ISC", "MIT", "OBSD", "Public Domain (No license)"]
+            choices: ["Apache", "GNU", "MIT", "BSD", "None"]
         },
         {
             type: "input",
@@ -44,20 +45,43 @@ const questions = () => {
         },
         {
             type: "input",
-            message: "GitHub Repo URL",
-            name: 'GitHubRepo',
+            message: "GitHub Username",
+            name: 'GitHubUsername',
+        },
+        {
+            type: 'input',
+            message: 'Please enter your email address here:',
+            name: 'email'
         },
     ])
 };
 
 // TODO: Create a function to write README file
 
-function writeToFile(fileName, data) {
+/*
+ * It takes two arguments, a filename and some data, and writes the data to the file
+ * filename - The name of the file you want to create.
+ * data - The data to write to the file.
+ */
+function writeToFile(filename, data) {
+    fs.writeFile(filename, data, (error) => error ? console.log(error) : console.log('Success! README File created'));
+} 
 
-}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+/*
+ * The function init() is an asynchronous function that uses the inquirer package to prompt the user
+ * for information based on the questions. The user's responses are then passed into the generateMarkdown() function, which
+ * returns a string of markdown text. The markdown text is then written to a file called README.md.
+ */
+async function init() {
+    let responses = await inquirer.prompt(questions);
+
+    let readMe = generateMarkdown(responses)
+
+    fs.writeFile('README.md', readMe, (error) => error ? console.log(error) : console.log('Success! README.md has been created'));
+}
 
 // Function call to initialize app
 init();
